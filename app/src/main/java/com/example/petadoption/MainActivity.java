3384d477fd46d28f44f3dc5605e281524ae1d9e4
Jason Nickell <43504239+data10019@users.noreply.github.com>
2022-05-03 12:42:09 -0700

    attached editprofile
    added editprofile and button functionality

--- app/src/main/java/com/example/petadoption/MainActivity.java
+++ app/src/main/java/com/example/petadoption/MainActivity.java
@@ -11,10 +11,14 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button buttonLogOut;
+    private Button leftBtn, middleLeftBtn, middleRightBtn, rightBtn,btnChangePfp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        leftBtn = findViewById(R.id.leftBtn);
+        leftBtn.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, EditProfileActivity.class));
+            }
+        });
+
+        middleLeftBtn = findViewById(R.id.middleLeftBtn);
+        middleLeftBtn.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+/*
+        middleRightBtn = findViewById(R.id.middleRightBtn);
+        middleRightBtn.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity.class)); // edit when get post viewer scene
+            }
+        });
+
+        rightBtn = findViewById(R.id.rightBtn);
+        rightBtn.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity.class)); // edit when get create post scene
+            }
+        });
+
+        btnChangePfp = findViewById(R.id.btnChangePfp);
+        btnChangePfp.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity.class)); // edit when get chat system
+            }
+        });*/
+
     }
 
     @Override
@@ -44,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
         }
+
+
     }
     public void logout()
     {
