3fd4c74f8c2c4643cbc530ab1e404b580607e5a5
Jason Nickell <43504239+data10019@users.noreply.github.com>
2022-05-08 20:58:09 -0700

    combined new post page
    need to work on fixing the overall look of everything

---
+++ app/src/main/java/com/example/petadoption/CropperActivity.java
@@ -0,0 +1,62 @@
+package com.example.petadoption;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.util.UUID;
+
+public class CropperActivity extends AppCompatActivity {
+
+
+    String result;
+    Uri fileUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cropper);
+
+        readIntent();
+
+        String dest_uri = new StringBuilder(UUID.randomUUID().toString()).append(".jpg").toString();
+
+        UCrop.Options options = new UCrop.Options();
+
+        UCrop.of(fileUri,Uri.fromFile(new File(getCacheDir(),dest_uri)))
+                .withOptions(options)
+                .withAspectRatio(1,1)
+                .useSourceImageAspectRatio()
+                .withMaxResultSize(1000,1000)
+                .start(CropperActivity.this);
+    }
+
+    private void readIntent() {
+        Intent intent=getIntent();
+        if(intent.getExtras()!=null){
+            result=intent.getStringExtra("DATA");
+            fileUri=Uri.parse(result);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && requestCode==UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("RESULT",resultUri+"");
+            setResult(-1,returnIntent);
+            finish();
+        }
+        else if (resultCode==UCrop.RESULT_ERROR){
+            final Throwable cropError = UCrop.getError(data);
+        }
+    }
+}
