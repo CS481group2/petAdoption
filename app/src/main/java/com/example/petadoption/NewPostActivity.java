fc354eaa594ac3e0da7d1d946c3a75a44090f066
Thofire <46273054+Thofire@users.noreply.github.com>
2022-05-30 22:08:18 -0700

    ScrollView added
    NewPost ScrollView added to description

--- app/src/main/java/com/example/petadoption/NewPostActivity.java
+++ app/src/main/java/com/example/petadoption/NewPostActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
